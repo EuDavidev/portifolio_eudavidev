ece0a24c3071e554c8c2f4a603fc6a8a4a5e5a50
Davvwi <davisouza128@gmail.com>
2026-02-17 17:45:15 -0300

    feat: add new `Footer` component with dark mode support, contact information, and social media links.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -17,7 +17,7 @@ export const Footer = ({isDarkMode}) => {
         <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6 '>
             <p>© 2025 Davi Souza. Todos os direitos reservados.</p>
             <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-                <li><a target='_blank' href="https://github.com/Davvwi/">GitHub</a></li>
+                <li><a target='_blank' href="https://github.com/EuDavidev/">GitHub</a></li>
                 <li><a target='_blank' href="https://www.linkedin.com/in/davi-souza-075540309/">LinkedIn</a></li>
                 <li><a target='_blank' href="https://www.instagram.com/eudavidev/">Instagram</a></li>
             </ul>
